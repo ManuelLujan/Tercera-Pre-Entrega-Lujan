bf59adc83ee61ccf50b99da4c92445215c463730
Manuel Lujan <mleolujang@gmail.com>
2023-08-28 17:32:46 -0300

    casi finalizado

--- AppManuel/ProyectoCoder/admin.py
+++ AppManuel/ProyectoCoder/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Monotributo, Profesion, Vencimientos
 # Register your models here.
+
+admin.site.register(Monotributo)
+admin.site.register(Profesion)
+admin.site.register(Vencimientos)
