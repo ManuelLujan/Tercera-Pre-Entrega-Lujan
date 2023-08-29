7a6edeba1b647d95b3eba9a4668cf6a7fa8a3677
Manuel Lujan <mleolujang@gmail.com>
2023-08-29 14:27:57 -0300

    sexta parte

--- AppManuel/ProyectoCoder/forms.py
+++ AppManuel/ProyectoCoder/forms.py
@@ -5,4 +5,13 @@ class monotributoForm(forms.Form):
     nombre=forms.CharField(max_length=50)
     apellido=forms.CharField(max_length=50)
     cuit=forms.IntegerField()
-   
+
+class profesionForm(forms.Form):
+    actividad=forms.CharField(max_length=50)
+    moneda=forms.CharField(max_length=50)
+    ingresos_brutos=forms.IntegerField()
+
+class venciminetosForm(forms.Form):
+    cuit=forms.IntegerField()
+    fecha_vencimiento=forms.DateField()
+    pagaDeServicio=forms.BooleanField()
--- AppManuel/ProyectoCoder/views.py
+++ AppManuel/ProyectoCoder/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Monotributo, Profesion, Vencimientos
 from django.http import HttpResponse    
-from .forms import monotributoForm
+from .forms import monotributoForm, profesionForm, venciminetosForm
 # Create your views here.
 
 
@@ -10,18 +10,43 @@ def inicio(request):
     
 def monotributistas(request):
     if request.method=="POST":
+        formulario_mono=monotributoForm(request.POST)
+        if formulario_mono.is_valid():
+            data= formulario_mono.cleaned_data
+            monotributo= Monotributo(nombre=data["nombre"], apellido=data["apellido"], cuit=data["cuit"])
+            monotributo.save()
+            return redirect("monotributistas")
 
-        pass
+        
     else:
         formulario_mono=monotributoForm()
     monotributistas= Monotributo.objects.all()
     return render(request,"AppCoder/monotributo.html", {"formulario": formulario_mono})
 
 def profesiones(request):
-    return render(request,"AppCoder/profesion.html")
+    if request.method=="POST":
+        formulario_prof=profesionForm(request.POST)
+        if formulario_prof.is_valid():
+            dataP= formulario_prof.cleaned_data
+            profesion= Profesion(actividad=dataP["actividad"], moneda=dataP["moneda"] , ingresos_brutos=dataP["ingresos_brutos"])
+            profesion.save()
+            return redirect ("profesiones")
+    else:
+        formulario_prof=profesionForm()
+        return render(request, "AppCoder/profesion.html", {"FormP": formulario_prof})
+
 
 def vencimientos_servicio(request):
-    return render(request,"AppCoder/venc.html")
+    if request.method=="POST":
+        formulario_venc=venciminetosForm(request.POST)
+        if formulario_venc.is_valid():
+            dataF= formulario_venc.cleaned_data
+            vencimientos= Vencimientos(cuit=dataF["cuit"], fecha_vencimiento=dataF["fecha_vencimiento"] , pagaDeServicio=dataF["pagaDeServicio"])
+            vencimientos.save()
+            return redirect ("vencimientos_servicio")
+    else:
+        formulario_venc=venciminetosForm()
+        return render(request, "AppCoder/venc.html", {"FormF": formulario_venc})
 
 
 
